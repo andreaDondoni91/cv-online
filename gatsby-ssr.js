41a4244e230a58fb3f9ab7a51065536f6fa995a7
Andrea Dondoni <andrea.dondoni91@gmail.com>
2026-07-29 22:11:55 +0200

    fix favicon

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,8 +8,10 @@ export const onRenderBody = ({ pathname, setHeadComponents, setHtmlAttributes })
   setHtmlAttributes({ lang });
 
   setHeadComponents([
+    <link key="favicon-ico" rel="icon" href="/favicon.ico" sizes="48x48" />,
     <link key="favicon-svg" rel="icon" type="image/svg+xml" href="/favicon.svg" />,
-    <link key="favicon-png" rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />,
+    <link key="favicon-96" rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />,
+    <link key="favicon-32" rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />,
     <link key="apple-touch-icon" rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />,
   ]);
 };
